7a36986e4e47b644dc9e30caa3e7b5863cf102ba
Mehrdad <mehrdad@getubo.com>
2023-03-08 14:48:31 -0800

    updated example and readme

--- rgb_ring/README.md
+++ rgb_ring/README.md
@@ -3,14 +3,18 @@ This repo includes code for interfacing with Ubo RGB LED Ring.
 
 # Client/Manager Model
 The RGB Ring is controlled and managed by `rgb_ring_manager.py`. The manager 
-runs as a system service with root previlage. Since Ubo uses Neopixels and due to
-the fact that Neopixel LEDs are driven through DRM, the script requires `sudo` previlage 
+runs as a system service with root previlage. Since the ring manager service uses Neopixel driver 
+and due to the fact that Neopixel LEDs are driven through DRM, the script requires root `sudo` previlage 
 to run.
 
 To eliminate the need to run all applications that want to communicate with
 RGB LED ring run with root previlage, we run the manager with root and other
 applications send messages to it via writing to a socket. The manager constantly
 read the incoming messages at that socket and displays the communicated pattern.
+
+
+Please note that disable sound card
+The install script (`system/setup/install.sh`) automically adds the rgb-ring service
 
 
 ```
@@ -20,26 +24,64 @@ sudo systemctl restart rgb-ring
 sudo systemctl enable rgb-ring
 ```
 
-
-The install script (`/setup/install.sh`) automically adds the rgb-ring service
-
-# 
-
 2) permission model
 
 Only applications that belong to the user group that have write permission to that 
 socket can send patterns to led ring manager.
 
+file socket `ledmanagersocket.sock`
 
 3) setting up the service (add to install script)
+
+`sudo bash system/setup/start_services.sh`
+
 4) document primitives
 
-Pulse
-Glow
-Blink
-Solid
-Fill Up to
-Fill Down to
+```
+lc = LEDClient()
+lc.set_enabled(True)
+
+# sets all LEDs to the specified color
+lc.set_all(color=(255, 255, 255))
+
+
+lc.blank()
+
+lc.set_brightness(0.5)
+
+lc.fill_upto(color = (255,255,255), 
+            percentage = 0.5,
+            wait = 100)
+
+lc.fill_downfrom(color = (255,255,255), 
+            percentage = 0.5,
+            wait = 100)
+
+
+Glows the LEDs in a rainbow pattern
+percentage: is a float between 0 and 1
+wait: is in milliseconds
+
+lc.rainbow(rounds=10, wait=5)
+
+# increments the position of bright strip by one step.
+lc.progress_wheel_step(color=(0, 255, 0))
+
+
+lc.pulse(color = (255,0,255), 
+        wait = 50, #in miliseconds
+        repetitions = 5)
+
+# Spinning wheel 
+# length = 10 leds
+lc.spinning_wheel(color = (255,255,255), 
+                wait = 20, #miliseconds
+                length = 10, #leds
+                repetitions = 3)
+    
+
+
+```
 
 5) add rgb_ring_example.py
 
--- rgb_ring/rgb_ring_example.py
+++ rgb_ring/rgb_ring_example.py
@@ -2,110 +2,127 @@ from rgb_ring_client import LEDClient
 import time
 
 # Instantiate an LED Ring client
-try:
-    lc = LEDClient()
-    lc.set_enabled(True)
-    lc.set_all((255, 255, 255))
+lc = LEDClient()
+# Enable ROG LED Ring
+lc.set_enabled(True)
+# Set all LEDs to white color
+lc.set_all(color=(255, 255, 255))
+time.sleep(1)
+print("White ring must glow for 1 second")
 
-    time.sleep(1)
-    print("White ring must glow for 1 second")
+# This line will disable the LED
+# ring and sending any commands after that
+# does not have an effect 
+lc.set_enabled(False)
+lc.set_all((255, 0, 0))
+print("red ring must NOT glow for 1 second")
+time.sleep(1)
+# enabling RGB LED ring again
+lc.set_enabled(True)
 
-    # This line will disable the LED
-    # ring and everything after that
-    # does not take effect
-    lc.set_enabled(False)
-    lc.set_all((255, 0, 0))
-    print("red ring must NOT glow for 1 second")
-    time.sleep(1)
+#set RGB ring to blank state
+lc.blank()
 
-    # enabling RGB LED ring again
-    lc.set_enabled(True)
+# fill up the ring up to a given LED index
+lc.fill_upto(color=(0,0,255), 
+            percentage=1, #fill up to 100%
+            wait=20) #miliseconds
+time.sleep(1)
 
-    #set RGB ring to blank state
-    lc.blank()
+# fill down the rgb ring from a given LED index
+lc.fill_downfrom(color=(0,0,255), 
+                percentage=1, #fill down from full ring / 100%
+                wait=20) #in miliseconds
+time.sleep(1)
 
-    # fill up the ring up to a given LED index
-    lc.fill_upto((0,0,255), 1, 20)
-    time.sleep(1)
+# Magenta
+# Set all leds to magenta color 
+lc.set_all(color=(255, 0, 255))
+time.sleep(1)
 
-    # fill down the rgb ring from a given LED index
-    lc.fill_downfrom((0,0,255), 1, 20)
-    time.sleep(1)
+# Yellow
+# Set all leds to yellow color 
+lc.set_all(color=(255, 255, 0))
+time.sleep(1)
 
-    # Magenta
-    # Set all leds to magenta color 
-    lc.set_all((255, 0, 255))
-    time.sleep(1)
+# Cyan
+# Set all leds to Cyan color 
+lc.set_all(color=(0, 255, 255))
+time.sleep(1)
 
-    # Yellow
-    # Set all leds to yellow color 
-    lc.set_all((255, 255, 0))
-    time.sleep(1)
+# White
+# Set all leds to White color 
+lc.set_all(color=(255, 255, 255))
+time.sleep(1)
 
-    # Cyan
-    # Set all leds to Cyan color 
-    lc.set_all((0, 255, 255))
-    time.sleep(1)
+# Red
+# Set all leds to Red color 
+lc.set_all(color=(255, 0, 0))
+time.sleep(1)
 
-    # White
-    # Set all leds to White color 
-    lc.set_all((255, 255, 255))
-    time.sleep(1)
+# Green
+# Set all leds to Green color 
+lc.set_all(color=(0, 255, 0))
+time.sleep(1)     
 
-    # Red
-    # Set all leds to Red color 
-    lc.set_all((255, 0, 0))
-    time.sleep(1)
+# Blue
+# Set all leds to Blue color 
+lc.set_all(color=(0, 0, 255))
+time.sleep(1)
 
-    # Green
-    # Set all leds to Green color 
-    lc.set_all((0, 255, 0))
-    time.sleep(1)     
+# Turn off all leds
+lc.blank()
 
-    # Blue
-    # Set all leds to Blue color 
-    lc.set_all((0, 0, 255))
-    time.sleep(1)
+# Activate rainbow
+# wait: milisec
+lc.rainbow(rounds=10, 
+            wait=5) 
+time.sleep(1)
 
-    # Turn off all leds
-    lc.blank()
+# lower ring brightness
+for i in range(0, 21, 1):
+    lc.set_all(color=(255, 255, 255))
+    lc.set_brightness(i/20)
+    time.sleep(0.2)
 
-    # Activate rainbow
-    lc.rainbow(10, 5)
-    time.sleep(1)
-    for i in range(0, 11, 1):
-        lc.set_brightness(i/10)
-        time.sleep(0.2)
+# Show progress wheel step
+for i in range(15):
+    lc.progress_wheel_step(color=(0, 255, 0))
+    time.sleep(0.1)
 
-    # Show progress wheel step
-    for i in range(15):
-        lc.progress_wheel_step((0, 255, 0))
-        time.sleep(0.1)
+# Pulse
+lc.pulse(color = (255,0,255), 
+        wait = 50, #in miliseconds
+        repetitions = 5)
 
-    # Pulse
-    lc.pulse(color = (255,0,255), 
-            wait = 50, #in miliseconds
-            repetitions = 5)
+time.sleep(2)
 
-    time.sleep(2)
+# Blink
+lc.blink(color=(255, 0, 0), 
+        wait=200, #turn off for miliseconds in between
+        repetions=5) # blink 5 times
+time.sleep(1)
 
-    # Blink
-    lc.blink((255, 0, 0), 200, 5)
-    time.sleep(1)
+# Spinning wheel 
+# length = 10 leds
+lc.spinning_wheel(color = (255,255,255), 
+                wait = 20, #miliseconds
+                length = 10, #number of leds, maxes to 27
+                repetitions = 3) #spin 3 times
+time.sleep(1)
+lc.blank()
 
-    # Spinning wheel
-    lc.spinning_wheel((255, 255, 255), 20, 10, 3)
-    time.sleep(2)
-    lc.blank()
+# length = 1 led
+lc.spinning_wheel(color = (255,255,255), 
+                wait = 20, #miliseconds
+                length = 1, #leds
+                repetitions = 3)
+time.sleep(1)
+lc.blank()
 
-    # Set progress wheel to 60%
-    for i in range(25):
-        lc.progress_wheel((0, 0, 255), i/25)
-        time.sleep(0.1)
-    lc.blank()
-    time.sleep(1)
-    lc.rainbow(3, 5)
-    time.sleep(2)
-    lc.blank()
-except:
-    print("rgb ring manager service is not running or permission problem")
+# Set progress wheel to 60%
+for i in range(25):
+    lc.progress_wheel(color = (255,255,255), 
+                    percentage = i/25):
+    time.sleep(0.1)
+lc.blank()
