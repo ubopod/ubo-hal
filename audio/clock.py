de8e8397fd81a1c4de95b27b1b1670a4030d2cd4
xbeau <hi@xbea.us>
2023-03-15 15:06:19 -0700

    Speaks time, added one beep per hour

--- audio/clock.py
+++ audio/clock.py
@@ -25,8 +25,8 @@ from time import sleep
 
 try:
     s = pyttsx3.init()
-    s.say("The time is")
-    s.runAndWait()
+    #s.say("The time is")
+    #s.runAndWait()
 except:
     print("ERROR: is espeak installed?\n Use 'sudo apt install espeak'")
     exit(1)
@@ -38,14 +38,21 @@ print(now)
 time = str(now).split(":")
 
 # Play the recorded audio file for hour
-#os.system("aplay -D plughw:CARD=seeed2micvoicec,DEV=0 -c 1 -f S16_LE " + str(int(time[0])) + ".wav")
+os.system("aplay -D plughw:CARD=seeed2micvoicec,DEV=0 -c 1 -f S16_LE " + str(int(time[0])) + ".wav")
 
 if int(time[0]) > 12:
     time[0] = str(int(time[0]) - 12)
+
+
+for x in range(int(time[0])):
+    os.system("aplay -D plughw:CARD=seeed2micvoicec,DEV=0 -c 1 -f S16_LE beep.wav")
+
+
 
 hour = AudioSegment.from_wav(str(int(time[0])) + ".wav")
 play(hour)
 
 # Use espeak to say the minutes of the hour
+#s.say("%s, %s"%(str(int(time[0])),str(int(time[1]))))
 s.say("%s"%(str(int(time[1]))))
 s.runAndWait()
