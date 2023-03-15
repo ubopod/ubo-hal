7e5acd2bac018b5031d7be783d3a1b5ebf3e43ad
xbeau <hi@xbea.us>
2023-03-15 12:15:10 -0700

    record.py utility for hours, uses pydub to speak hour and espeak for rest

--- audio/clock.py
+++ audio/clock.py
@@ -3,7 +3,8 @@
 # Pre-requisites:
 #   sudo apt install espeak
 #   pip3 install pyttsx3
-
+#
+# TODO: check output is set to internal speakers, and reasonable volume
 
 try:
     import pyttsx3
@@ -11,16 +12,37 @@ except:
     print("ERROR: is pyttsx3 installed?\nUse 'pip3 install pyttsx3'")
     exit(1)
 
-from datetime import datetime
+try:
+    from pydub import AudioSegment
+    from pydub.playback import play
+except:
+    print("ERROR: is pydub installed?]nUse 'pip3 install pydub'")
+    exit(1)
 
-s = pyttsx3.init()
-s.say("The time is")
-s.runAndWait()
+import os
+from datetime import datetime
+from time import sleep
+
+try:
+    s = pyttsx3.init()
+    s.say("The time is")
+    s.runAndWait()
+except:
+    print("ERROR: is espeak installed?\n Use 'sudo apt install espeak'")
+    exit(1)
+
 
 now = datetime.now().time()
 print(now)
 
 time = str(now).split(":")
 
-s.say("%s, %s"%(str(int(time[0])), str(int(time[1]))))
+# Play the recorded audio file for hour
+#os.system("aplay -D plughw:CARD=seeed2micvoicec,DEV=0 -c 1 -f S16_LE " + str(int(time[0])) + ".wav")
+
+hour = AudioSegment.from_wav(str(int(time[0])) + ".wav")
+play(hour)
+
+# Use espeak to say the minutes of the hour
+s.say("%s"%(str(int(time[1]))))
 s.runAndWait()
