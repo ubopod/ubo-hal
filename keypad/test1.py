d2ff68087d8a463f55f7e23d0d6b574a7dab4e2b
jeanpierre Sainfeld <jean-pierre.sainfeld@firstlinkconsulting.com>
2023-03-31 15:55:24 -0700

    Added ubo_logger.py and test1.py to validate logging

---
+++ keypad/test1.py
@@ -0,0 +1,35 @@
+import sys
+import os
+
+up_dir = os.path.dirname(os.path.abspath(__file__)) + '/../utils'
+sys.path.append(up_dir)
+
+from ubo_logger import *
+#import utils.ubo_logger
+
+
+def main(argv: object):
+    """
+    Main function
+    - instantiate KEYPAD class
+    """
+
+    # ==================================
+    # Establish the logging mechanism
+    # local_log is true if we want 
+    #  
+    # ==================================
+    local_log: bool = False
+    ubo_logger.establish_logging(local_log)
+
+
+if __name__ == '__main__':
+    try:
+        main(sys.argv[1:])
+
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
