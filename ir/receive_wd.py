b490fb4859382181789872f31a79123ba2f1068c
Jean-Pierre Sainfeld <jp.sainfeld@firstlinkconsulting.com>
2023-04-10 16:47:13 -0700

    Fixed the path to the lcd

--- ir/receive_wd.py
+++ ir/receive_wd.py
@@ -6,7 +6,7 @@ import os
 import sys
 import time
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__))+'../')
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../')
 
 from display.lcd import LCD as LCD
 
