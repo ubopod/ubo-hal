23b65b880f6f6caa57233de7e68feba74f3d906b
Mehrdad M <mehrdad@getubo.com>
2023-08-18 16:37:06 -0700

    code clean up and refacoring #1

--- ubo_keypad/ubo_keypad.py
+++ ubo_keypad/ubo_keypad.py
@@ -1,65 +1,79 @@
 import RPi.GPIO as GPIO
 from adafruit_bus_device import i2c_device
 import adafruit_aw9523
-from PIL import Image, ImageDraw, ImageFont
 import logging.config
 import board
 import math
 import time
-import signal
 import os
 import sys
 
 SDK_HOME_PATH = os.path.dirname(os.path.abspath(__file__)) + '/../'
 sys.path.append(SDK_HOME_PATH)
 
-# try:
-#     from self.configparser import configparser
-# except ImportError:
-#     import configparser
-
-# CONFIG_FILE = './config/config.ini'
-# STATUS_FILE = './info/status.ini'
 LOG_CONFIG = SDK_HOME_PATH + "system/log/logging-debug.ini"
 logging.config.fileConfig(LOG_CONFIG,
                           disable_existing_loggers=False)
+
 INT_EXPANDER = 5 # GPIO PIN index that receives interrupt from AW9523
 
 class KEYPAD(object):
-
+    """ Class to handle keypad events.
+        "0": Top left button
+        "1": Middle left button 
+        "2": Bottom left button
+        "back": Button with back arrow label (under LCD)
+        "home": Button with home label (Under LCD)
+        "up": Right top button with upward arrow label
+        "down": Right bottom button with downward arrow label
+        "mic": Microphone mute switch
+    """
     def __init__(self, *args, **kwargs):
         """
         KEYPAD Constructor
         This initializes various parameters including
         loggers and button names.
         """
-        # self.config = configparser.ConfigParser()
-        # self.config.read(CONFIG_FILE)
-        # self.status = configparser.ConfigParser()
-        # self.status.read(STATUS_FILE)
         self.logger = logging.getLogger("keypad")
-        self.display_active = False
-        self.window_stack = []
-        self.led_enabled = True
         self.logger.debug("Initialising keypad...")
         self.aw = None
         self.mic_switch_status = False
         self.last_inputs = None
         self.bus_address = False
         self.model = "aw9523"
-        self.init_i2c()
         self.enabled = True
-        # "0": Top left button
-        # "1": Middle left button 
-        # "2": Bottom left button
-        # "back": Button with back arrow label (under LCD)
-        # "home": Button with home label (Under LCD)
-        # "up": Right top button with upward arrow label
-        # "down": Right bottom button with downward arrow label
         self.BUTTONS = ["0", "1", "2", "up", "down", "back", "home", "mic"]
         self.index = 0
         self.buttonPressed = self.BUTTONS[self.index]
+        self.init_i2c()
 
+    def reset_interrupt_flags(self, i2c):
+        # Write to both registers to reset the interrupt flag
+        buffer = bytearray(2)
+        buffer[0] = 0x00
+        buffer[1] = 0x00
+        i2c.write(buffer)
+        i2c.write_then_readinto(buffer, buffer, out_end=1, in_start=1)
+        #print(buffer)
+        time.sleep(0.1)
+        buffer[0] = 0x01
+        buffer[1] = 0x00
+        i2c.write(buffer)
+        i2c.write_then_readinto(buffer, buffer, out_end=1, in_start=1)
+        time.sleep(0.1)
+
+    def disable_interrupt_for_higher_bits(self, i2c):
+        # disable interrupt for higher bits
+        buffer = bytearray(2)
+        buffer[0] = 0x06
+        buffer[1] = 0x00
+        i2c.write(buffer)
+        i2c.write_then_readinto(buffer, buffer, out_end=1, in_start=1)
+        #print(buffer)
+        buffer[0] = 0x07
+        buffer[1] = 0xff
+        i2c.write(buffer)
+        i2c.write_then_readinto(buffer, buffer, out_end=1, in_start=1)
 
     def init_i2c(self):
         GPIO.setmode(GPIO.BCM)
@@ -84,56 +98,22 @@ class KEYPAD(object):
         time.sleep(1)
         # The code below, accessing the GPIO expander registers directly via i2c 
         # was created as a workaround of the reset the interrupt flag.
-        # First write to both registers to reset the interrupt flag
-        buffer = bytearray(2)
-        buffer[0] = 0x00
-        buffer[1] = 0x00
-        new_i2c.write(buffer)
-        new_i2c.write_then_readinto(buffer, buffer, out_end=1, in_start=1)
-        #print(buffer)
-        time.sleep(0.1)
-        buffer[0] = 0x01
-        buffer[1] = 0x00
-        new_i2c.write(buffer)
-        new_i2c.write_then_readinto(buffer, buffer, out_end=1, in_start=1)
-        #print(buffer)
-        # disable interrupt for higher bits
-        buffer[0] = 0x06
-        buffer[1] = 0x00
-        new_i2c.write(buffer)
-        new_i2c.write_then_readinto(buffer, buffer, out_end=1, in_start=1)
-        #print(buffer)
-        buffer[0] = 0x07
-        buffer[1] = 0xff
-        new_i2c.write(buffer)
-        new_i2c.write_then_readinto(buffer, buffer, out_end=1, in_start=1)
-        #print(buffer)
-        # read registers again to reset interrupt
-        buffer[0] = 0x00
-        buffer[1] = 0x00
-        new_i2c.write(buffer)
-        new_i2c.write_then_readinto(buffer, buffer, out_end=1, in_start=1)
-        #print(buffer)
-        time.sleep(0.1)
-        buffer[0] = 0x01
-        buffer[1] = 0x00
-        new_i2c.write(buffer)
-        new_i2c.write_then_readinto(buffer, buffer, out_end=1, in_start=1)
-        #print(buffer)
-        time.sleep(0.1)
 
-        for i in range(1):
-            self.last_inputs = self.aw.inputs
-            self.logger.debug("Inputs: {:016b}".format(self.last_inputs))
-            # print(self.last_inputs & 0x80)
-            self.mic_switch_status = ((self.last_inputs & 0x80) == 128)
-            self.logger.debug("mic switch is " + str(self.mic_switch_status))
-            time.sleep(0.5)
+        self.reset_interrupt_flags(new_i2c)
+        self.disable_interrupt_for_higher_bits(new_i2c)
+        # reset interrupts again
+        self.reset_interrupt_flags(new_i2c)
+        # read register values
+        self.last_inputs = self.aw.inputs
+        self.logger.debug("Inputs: {:016b}".format(self.last_inputs))
+        # read register value that correspods to microphone switch
+        self.mic_switch_status = ((self.last_inputs & 0x80) == 128)
+        self.logger.debug("mic switch is " + str(self.mic_switch_status))
         time.sleep(0.5)
+        # Enable interrupt on the GPIO expander
         GPIO.add_event_detect(INT_EXPANDER, GPIO.FALLING, callback=self.key_press_cb)
-        #GPIO.add_event_detect(INT_EXPANDER, GPIO.BOTH, callback=self.key_press_cb, bouncetime=200)
 
-    def key_press_cb(self,channel):
+    def key_press_cb(self,_channel):
         # This is callback funtion that gets triggers
         # if any change is detected on keypad buttons 
         # States. In this callback, we look at the state
@@ -142,27 +122,30 @@ class KEYPAD(object):
         self.logger.debug("Inputs: {:016b}".format(self.last_inputs))
         inputs = 127 - self.last_inputs & 0x7F
         # if input is 0, then only look at microphone
-        # switch state change
+        # switch state change.
         if inputs == 0:
-            """
-            When we reach this part of the code, it means
-            some changes in GPIO expander triggered an
-            interrupt but it was not due to a press on 
-            any of the keypad buttons. Here we see
-            if the interrupt was due to the change in
-            microphone muute switch status. Other non
-            keypad button events must also be handled here.
-            """
-            self.logger.debug("no keypad change")
+            # Reaching here means some changes in GPIO expander 
+            # triggered an interrupt but it was not due to a press on 
+            # any of the keypad buttons. 
+            # 
+            # The code below checks if the interrupt was due to 
+            # a change in microphone mute switch status. 
+            # 
+            # Other non-keypad button events must also be handled here.
+            self.logger.debug("No keypad change.")
+            # Look at value of register bit connnected to microphone switch
+            # to see if it has changed. 
+            # 
+            # Toggle the mic switch status accordingly.
             if ((self.last_inputs & 0x80) == 0) and \
-                (self.mic_switch_status == True):
+                (self.mic_switch_status is True):
                 self.logger.debug("Mic Switch is now OFF")
                 self.index = 7
                 self.buttonPressed = self.BUTTONS[self.index]
                 self.mic_switch_status = False
                 self.button_event()
             if ((self.last_inputs & 0x80) == 128) and \
-                (self.mic_switch_status == False):
+                (self.mic_switch_status is False):
                 self.logger.debug("Mic Switch is now ON")
                 self.index = 7
                 self.buttonPressed = self.BUTTONS[self.index]
@@ -198,7 +181,6 @@ class KEYPAD(object):
 
     def button_event(self):
         pass
-
 
 
 def main():
