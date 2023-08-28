b9c03a1f1070edc28ff4ab2b72372426a025e35a
Mehrdad M <mehrdad@getubo.com>
2023-08-28 14:25:11 -0700

    updated wpa_cli and refactored it

--- device/wpa_cli.py
+++ device/wpa_cli.py
@@ -1,24 +1,66 @@
 """
 examples of cli commands:
-> wpa_cli list_networks # -> returns list of configured networks
-> wpa_cli enable_network id # -> enables network
-> wpa_cli disable_network id # -> disables network 
-> wpa_cli remove_network id # -> removes network from wpa_supplicant.conf
-> wpa_cli save_config # -> saves wpa_supplicant.conf
-> wpa_cli reconfigure # -> reloads wpa_supplicant.conf
-> wpa_cli status # -> returns status of current connection
-> wpa_cli terminate # -> terminates wpa_supplicant
-> wpa_cli scan # -> scans for networks
-> wpa_cli scan_results # -> returns list of scanned networks
-> wpa_cli add_network # -> returns id of new network
-> wpa_cli set_network id ssid '"example_ssid"' # set ssid for WPA
-> wpa_cli set_network id psk '"example_password"' # set password for WPA
-> wpa_cli set_network id key_mgmt NONE # set key_mgmt for OPEN
-> wpa_cli set_network id wep_key '"example_password"' # set password for WEP
-> wpa_cli log_level DEBUG # -> sets log level to debug
+
+wpa_cli -i wlan0 + command
+command:
+> list_networks # -> returns list of configured networks
+> enable_network id # -> enables network
+> disable_network id # -> disables network 
+> remove_network id # -> removes network from wpa_supplicant.conf
+> save_config # -> saves wpa_supplicant.conf
+> reconfigure # -> reloads wpa_supplicant.conf
+> status # -> returns status of current connection
+> terminate # -> terminates wpa_supplicant
+> scan # -> scans for networks
+> scan_results # -> returns list of scanned networks
+> add_network # -> returns id of new network
+> set_network id ssid '"example_ssid"' # set ssid for WPA
+> set_network id psk '"example_password"' # set password for WPA
+> set_network id key_mgmt NONE # set key_mgmt for OPEN
+> set_network id wep_key '"example_password"' # set password for WEP
+> log_level DEBUG # -> sets log level to debug
+
+>> The list_networks command in wpa_cli returns a list of c onfigured network 
+blocks along with their network IDs. The columns returned by this command 
+include the network ID, SSID, BSSID, flags, and other information about the 
+networks.
+
+The "flags" column provides information about the status of each network 
+block. Some possible flag values that you might encounter include:
+
+[DISABLED]: Indicates that the network block is currently disabled.
+[CURRENT]: Indicates that the network block is the currently associated network.
+[TEMP-DISABLED]: Indicates that the network block is temporarily disabled. This could happen due to connection failures.
+[P2P-PERSISTENT]: Indicates that the network block is associated with a persistent P2P group.
+[P2P-GO]: Indicates that the network block is acting as the Group Owner in a P2P connection.
+[P2P-CLIENT]: Indicates that the network block is acting as a client in a P2P connection.
+[P2P-DEVICE]: Indicates that the network block is a P2P device.
+[P2P-FIND]: Indicates that the network block is in the process of finding P2P peers.
+[P2P-PD]: Indicates that the network block is in the process of provisioning a P2P device.
 
 For more information on wpa_cli commands, see:
 https://www.qnx.com/developers/docs/6.5.0SP1.update/com.qnx.doc.neutrino_utilities/w/wpa_cli.html
+http://hackerj.tistory.com/34
+https://linux.die.net/man/8/wpa_cli
+
+NOTES: 
+
+(1) please make sure your userspace belongs to the netdev group on Raspberry Pi. 
+Pi user is already a member of this group by default.
+
+(2) To address issue with error:
+
+CTRL-EVENT-ASSOC-REJECT status_code=16
+
+That leads to WiFi getting blacklisted, add the following line to 
+
+/etc/modprobe.d/brcmfmac.conf:
+
+options brcmfmac roamoff=1
+
+More info:
+https://raspberrypi.stackexchange.com/questions/77144/rpi3-wireless-issue-ctrl-event-assoc-reject-status-code-16/140525#140525
+
 """
 
 import subprocess
@@ -27,6 +69,7 @@ import logging.config
 from time import sleep
 import os
 import sys
+import re
 
 SDK_HOME_PATH = os.path.dirname(os.path.abspath(__file__)) + '/../'
 sys.path.append(SDK_HOME_PATH)
@@ -49,6 +92,30 @@ class WpaCliWrapper:
         self.interface = interface
         self.logger = logging.getLogger("wpa_cli")
     
+    def _run_system_command(self, command: Union[str, list[str]]) -> str:
+        """Run the cli command as system commands.
+
+        Parameters
+        ----------
+        command: Union[str, list[str]]
+            The command to run. Can be a string or a list of strings.
+            It uses the subprocess.run() method to run the command.
+        
+        Returns
+        -------
+        str: The output of the command.
+        """
+        try:
+            result = subprocess.run(command, 
+                                    stdout=subprocess.PIPE, 
+                                    stderr=subprocess.PIPE, 
+                                    text=True, 
+                                    check=True)
+            return result.stdout.strip()
+        except subprocess.CalledProcessError as e:
+            self.logger.debug( f"Error: {e.stderr.strip()}")
+
+
     def _run_command(self, 
                      command: Union[str, list[str]]) -> str:
         """Run the cli command as system commands.
@@ -66,16 +133,15 @@ class WpaCliWrapper:
         elif isinstance(command, list):
             cmd.extend(command)
         self.logger.debug(f'Running command: {cmd}')
-        try:
-            result = subprocess.run(cmd, 
-                                    stdout=subprocess.PIPE, 
-                                    stderr=subprocess.PIPE, 
-                                    text=True, 
-                                    check=True)
-            return result.stdout.strip()
-        except subprocess.CalledProcessError as e:
-            self.logger.debug( f"Error: {e.stderr.strip()}")
+        return self._run_system_command(cmd)
 
+    def generate_passphrase(self, ssid, password=None):
+        # wpa_passphrase "ssid" passphrase
+        cmd = ['wpa_passphrase', ssid, password]
+        network = self._run_system_command(cmd)
+        self.logger.debug(f'Generated network: {network}')
+        psk = self._parse_psk(network)
+        return psk
     
     def _parse_scan_results(self, results):
         """Parse the scan results into a list of dictionaries.
@@ -97,25 +163,25 @@ class WpaCliWrapper:
                 'ssid': ssid
             }
         """
-        lines = results.split('\n')[2:]
         networks = []
-
-        for line in lines:
-            values = line.split()
-            if len(values) >= 5:
-                bssid = values[0]
-                frequency = int(values[1])
-                signal_level = int(values[2])
-                flags = values[3].strip('[]').split('][')
-                ssid = values[4]
-                
-                networks.append({
-                    'bssid': bssid,
-                    'frequency': frequency,
-                    'signal_level': signal_level,
-                    'flags': flags,
-                    'ssid': ssid
-                })
+        if results:
+            lines = results.split('\n')[2:]            
+            for line in lines:
+                values = line.split()
+                if len(values) >= 5:
+                    bssid = values[0]
+                    frequency = int(values[1])
+                    signal_level = int(values[2])
+                    flags = values[3].strip('[]').split('][')
+                    ssid = values[4]
+                    
+                    networks.append({
+                        'bssid': bssid,
+                        'frequency': frequency,
+                        'signal_level': signal_level,
+                        'flags': flags,
+                        'ssid': ssid
+                    })
 
         return networks
 
@@ -139,6 +205,7 @@ class WpaCliWrapper:
                 'flags': flags
             }
         """
+        print(results)
         lines = results.split('\n')
         networks = []
         for line in lines:
@@ -151,6 +218,7 @@ class WpaCliWrapper:
                     'bssid': bssid,
                     'flags': flags
                 })
+        self.logger.debug(f'List of networks: {networks}')
         return networks
     
     def _parse_list_interfaces(self, results: str) -> list[str]:
@@ -211,6 +279,31 @@ class WpaCliWrapper:
             status[key] = value
         
         return status
+    
+    def _parse_psk(self, cmd_output: str) -> str:
+        """Parse the generate passphrase command results into a string.
+
+        Parameters
+        ----------
+        cmd_output: 'str'
+            The raw results from the generate_passphrase command.
+
+        Returns
+        -------
+        psk: 'str'
+            The psk string.
+        """
+        pattern = r'psk=(\w+)'
+        match = re.search(pattern, cmd_output)
+
+        if match:
+            psk_value = match.group(1)
+            print("Extracted PSK value:", psk_value)
+            return psk_value
+        else:
+            print("PSK value not found in the input.")
+            return None
+
     
     def scan(self):
         """Run the scan command."""
@@ -303,6 +396,14 @@ class WpaCliWrapper:
         """
         return self._run_command(['remove_network', f'{network_id}'])
 
+    def clear_blacklist(self):
+        """Run the clear_blacklist command."""
+        return self._run_command(['blacklist', 'clear'])
+    
+    def check_blacklist(self):
+        """Run the blacklist command."""
+        return self._run_command(['blacklist'])
+
     def terminate(self):
         """Run the terminate command."""
         return self._run_command('terminate')
@@ -331,163 +432,6 @@ class WpaCliWrapper:
         return self._run_command(['log_level', f'{level}'])
     # Add more methods as needed for other wpa_cli commands
 
-class wifiManager(WpaCliWrapper):
-    """
-    A class that inherits from WpaCliWrapper and adds methods to manage wifi connections
-
-    Attributes
-    ----------
-    interface: 'str'
-        The network interface name to use for
-        wpa_cli commands. Default is wlan0.
-    """
-    def __init__(self, interface='wlan0'):
-        """initialize the class with default interface wlan0.
-        
-        Parameters
-        ----------
-        interface: 'str'
-            The network interface name to use for
-            wpa_cli commands. Default is wlan0.
-        """
-        super().__init__(interface)
-        self.networks = self.list_networks()
-        self.interfaces = self.list_interfaces()
-        self.interface = interface
-        self.current_network = self.status()
-
-    def get_network_id(self, ssid: str) -> Union [str, None]:
-        """ Get the network id of a configured network
-        
-        Parameters
-        ----------
-        ssid: 'str'
-            The ssid of the network to get the network id for
-        
-        Returns
-        ----------
-        network_id: 'str'
-            The network id of the network with the given ssid
-        """
-        for network in self.networks:
-            if network.get('ssid') == ssid:
-                return network.get('network_id')
-        return None
-    
-    def get_latest_status(self) -> dict:
-        """ Get the latest status of the wifi connection
-
-        Returns
-        --------
-            current_network: 'dict' 
-            A dictionary containing the latest status of the wifi connection
-
-        Behavior
-        ---------
-            This method will check the status of the wifi connection every 2 seconds
-            Until status is not 'SCANNING' or 'ASSOCIATING' or 20 seconds have passed
-        """
-        self.current_network = self.status()
-        iterations = 10
-        while (self.current_network.get('wpa_state') == 'SCANNING' or \
-               self.current_network.get('wpa_state') == 'ASSOCIATING') \
-               and iterations > 0:
-            self.current_network = self.status()
-            self.logger.debug(f'Current status: {self.current_network}')    
-            sleep(2)
-            iterations -= 1
-        return self.current_network
-    def add_wifi(self, 
-                 ssid: str, 
-                 password: str, 
-                 type: Literal['WPA','WEP','OPEN','']
-                 ) -> Union[str, None]:
-        """ Add a wifi network to the list of configured networks.
-        
-        Parameters
-        ----------
-        ssid: 'str'
-            The ssid of the network to add
-        password: 'str'
-            The password of the network to add
-        type: Literal['WPA','WEP','OPEN','']
-            The type of the network to add. Default is WPA
-        
-        Returns
-        ----------
-        network_id: 'str'
-            The network id of the network that was added
-        """
-        try:
-            network_id = self.add_network()
-            self.logger.debug(f'network_id: {network_id}')
-            ssid_string = '"{}"'.format(ssid)
-            password_string = '"{}"'.format(password)
-            self.set_network(network_id, 'ssid', ssid_string)
-            if type == 'WPA':
-                self.set_network(network_id, 'psk', password_string)
-            elif type == 'WEP':
-                self.set_network(network_id, 'wep_key', password_string)
-            elif type == 'OPEN':
-                self.set_network(network_id, 'key_mgmt', 'NONE')
-            else:
-                self.logger.error("Unsupported type")
-            return network_id
-        except Exception as e:
-            self.logger.error(f'Error adding wifi: {e}')
-            return None
-
-    def connect_to_wifi(self, network_id: str) -> bool:
-        """ Connect to a wifi network
-        
-        Parameters
-        ----------
-        network_id: 'str'
-            The network id of the network to connect to
-
-        Returns
-        ----------
-        bool: True if the connection succeeded, False otherwise
-        """
-        if network_id:
-            try:
-                self.enable_network(network_id)
-                self.save_config()
-                self.reconfigure()
-                current_network = self.get_latest_status()
-                current_id = current_network.get('id')
-                current_ssid = current_network.get('ssid')
-                self.logger.debug(f'Current connected network ssid is: {current_ssid}')
-                if  current_id == network_id:
-                    self.logger.info(f'Connected to {current_ssid}')
-                    return True
-                else:
-                    self.logger.error("WiFi connection failed!")
-                    return False
-            except Exception as e:
-                self.logger.error(f'Error adding wifi: {e}')
-                return False
-        else:
-            self.logger.error("Network ID is None")
-            return False
-
-    def forget_wifi(self, ssid: str) -> bool:
-        """ Remove a wifi network from the list of configured networks.
-
-        Parameters
-        ----------
-        ssid: 'str'
-            The ssid of the network to remove
-
-        Returns
-        ----------
-        bool: True if the removal succeeded, False otherwise
-        """
-        network_id = self.get_network_id(ssid)
-        self.remove_network(network_id)
-        self.save_config()
-        self.reconfigure()
-        return True
 
 if __name__ == '__main__':
     wrapper = WpaCliWrapper()
@@ -504,13 +448,6 @@ if __name__ == '__main__':
     for interface in available_interfaces:
         print(interface)
 
-    wrapper.logger.info(wrapper.status())
-
-    W = wifiManager()
-    id = W.add_wifi(ssid='CircuitLaunch', password='makinghardwarelesshard', type='WPA')
-    R = W.connect_to_wifi(id)
-    W.logger.info("wifi connected: ", R)
-
-    configured_networks = wrapper.list_networks()
-    for network in configured_networks:
-         wrapper.logger.info(network)
+    # wrapper.logger.info(wrapper.status())
+    # psk = wrapper.generate_passphrase('earlplex-guest', 'hammerearlplex')
+    # print(psk)
