b61341f7dd2dcb1c343047ca088360d046f2e667
mehrdad <mehrdad@getubo.com>
2023-11-05 21:34:21 +0000

    avoid reinstalling eeprom

--- system/setup/install_eeprom.sh
+++ system/setup/install_eeprom.sh
@@ -1,7 +1,14 @@
-git clone https://github.com/raspberrypi/hats.git
-wget -c https://raw.githubusercontent.com/RobertCNelson/tools/master/pkgs/dtc.sh
-chmod +x dtc.sh
-./dtc.sh
-# then install these
-cd hats/eepromutils/
-make && sudo make install
+# Check if hats/eepromutils/ directory exists
+DIR_PATH=./hats/eepromutils/
+if [ -d "$DIR_PATH" ]; then
+    echo "The directory $DIR_PATH exists."
+else
+    echo "The directory $DIR_PATH does not exist."
+    git clone https://github.com/raspberrypi/hats.git
+    wget -c https://raw.githubusercontent.com/RobertCNelson/tools/master/pkgs/dtc.sh
+    chmod +x dtc.sh
+    ./dtc.sh
+    # then install these
+    cd hats/eepromutils/
+    make && sudo make install
+fi
